c34a388e6ecebef45c6d6454fdbc1bf68550c694
volausson <veronicagolausson@gmail.com>
2022-09-11 12:32:49 +0000

    add comment to models.py

--- blogg/models.py
+++ blogg/models.py
@@ -6,6 +6,7 @@ STATUS = ((0, 'Draft'), (1, 'Published'))
 
 # Created models.
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -21,10 +22,24 @@ class Post(models.Model):
     class Meta:
         ordering = ['-created_on']
 
-
     def __str__(self):
         return self.title
 
     def number_of_likes(self):
         return self.likes.count()
-        
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment {self.body} by {self.name}'
+
